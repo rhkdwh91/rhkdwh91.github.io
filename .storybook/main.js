39c34525552ea1f5d957706f3645c04fddbfd359
MacKwangyeon <rhkdwh91@gmail.com>
2023-07-03 18:39:31 +0900

    add

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,17 +1,23 @@
 /** @type { import('@storybook/react-vite').StorybookConfig } */
 const config = {
-  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-  ],
+  stories: ['../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions'],
+  core: {
+    builder: '@storybook/builder-vite', // 👈 The builder enabled here.
+  },
   framework: {
-    name: "@storybook/react-vite",
+    name: '@storybook/react-vite',
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: 'tag',
+  },
+  babel: async (options) => {
+    return {
+      ...options,
+      presets: ['@babel/preset-react'],
+      plugins: ['@babel/plugin-syntax-flow'],
+    };
   },
 };
 export default config;
